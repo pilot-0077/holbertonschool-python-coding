9734bf20aa41ff29205b9a6b558c9e78672370db
PJ <63235746+pilot-0077@users.noreply.github.com>
2026-01-12 12:26:17 +0400

    Add type checks for size in Square class

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -3,7 +3,11 @@
 
 
 class Square:
-    """Represent a square."""
+    """Represents a square."""
 
     def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
