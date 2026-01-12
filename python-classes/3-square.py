65c1823ba4ccd7c0f64f1e4beedceb2c70668769
PJ <63235746+pilot-0077@users.noreply.github.com>
2026-01-12 12:44:31 +0400

    Update 3-square.py

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python3
-"""Module for creating Square class"""
+"""Defines a Square class."""
 
 
 class Square:
-    """A class representing Square"""
+    """Represents a square."""
+
     def __init__(self, size=0):
-        """Instantiation with a given size"""
-        if not isinstance(size, int):
+        self.size = size  # setter validation here
+
+    @property
+    def size(self):
+        """Retrieve the size."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size with validation."""
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = value
 
     def area(self):
-        """Return of the area of square"""
-        return self.__size**2
+        """Return the area of the square."""
+        return self.__size * self.__size
